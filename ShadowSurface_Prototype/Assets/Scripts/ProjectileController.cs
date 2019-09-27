5cc21b5118a7eaff42270a2b6eef612f10970f55
devyaoyh <devyaoyh@gmail.com>
2019-09-27 17:32:05 -0500

    Init with top-down platformer

---
+++ ShadowSurface_Prototype/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody;
+    private int bounce_count = 2;
+    private float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(transform.right * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bounce_count--;
+        if (bounce_count < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
